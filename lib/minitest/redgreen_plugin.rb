8eda02ac47a4528415474dbdb61dfbe7c6e08b6d
Dom <hello@meetdom.com>
2014-04-08 11:36:07 +1000

    Added pluralization of Minitest output.
    Still need to add tests for this.

--- lib/minitest/redgreen_plugin.rb
+++ lib/minitest/redgreen_plugin.rb
@@ -43,15 +43,22 @@ module Minitest
       return io.puts if o.nil?
 
       io.puts o
-        .gsub(/\b ((\d+) \s+ failures?) \b/x) { Integer($2) > 0 ? failing($1) : passing($1) }
-        .gsub(/\b ((\d+) \s+ errors?)   \b/x) { Integer($2) > 0 ? erring($1)  : passing($1) }
-        .gsub(/\b ((\d+) \s+ skips?)    \b/x) { Integer($2) > 0 ? skipping($1): passing($1) }
+        .gsub(/\b ((\d+) \s+ runs?) \b/x) { pluralize($1) }
+        .gsub(/\b ((\d+) \s+ assertions?) \b/x) { pluralize($1) }
+        .gsub(/\b ((\d+) \s+ failures?) \b/x) { Integer($2) > 0 ? failing(pluralize($1)) : passing(pluralize($1)) }
+        .gsub(/\b ((\d+) \s+ errors?)   \b/x) { Integer($2) > 0 ? erring(pluralize($1))  : passing(pluralize($1)) }
+        .gsub(/\b ((\d+) \s+ skips?)    \b/x) { Integer($2) > 0 ? skipping(pluralize($1)): passing(pluralize($1)) }
     end
 
     delegate [:sync, :sync=] => :io
 
     private
 
+    def pluralize(input)
+      text = input.split(" ")
+      word = text[0].to_i == 1 ? "#{text[0]} #{text[1][0..-2]}" : "#{text[0]} #{text[1]}"
+    end
+    
     def failing(o); escape('31m') {o}; end
     def passing(o); escape('32m') {o}; end
     def skipping(o); escape('33m') {o}; end
